b6b66a6b6a458bb8c646a07cc88b86da85798142
ShristiJalan <shristijalan1010@gmail.com>
2021-06-07 16:41:34 +0530

    Binary to Decimal

--- c++/binaryToDecimal.cpp
+++ c++/binaryToDecimal.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int binaryToDecimal(int n)
 {
     int ans = 0;
     int x = 1;
+    int c = 0;
+    int sum = 0;
     while (n > 0)
     {
-        int y = n % 10;
-        ans += x * y;
-        x *= 2;
-        n /= 10;
+        int lastDigit = n % 10;
+        int temp = lastDigit * pow(2, c);
+        sum = sum + temp;
+        n = n / 10;
+        c++;
     }
+    return sum;
 }
 
 int main()
